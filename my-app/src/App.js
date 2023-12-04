198265deffb0cddbdd039edbd0c15b4772765371
Rob Killick <robkillick@me.com>
2023-12-04 14:58:55 +0000

    add conditional rendering components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 // import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 // import StatefulGreeting from './components/StatefulGreeting';
-import StatefulGreetingWithPrevState from './components/StatefulGreeting';
+// import StatefulGreetingWithPrevState from './components/StatefulGreeting';
+// import EventsFunctional from './components/EventsFunctional';
+// import EventsClass from './components/EventsClass';
+import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreetingWithProps greeting="nice to meet you" name="Rob" age="42" /> */}
       {/* <StatefulGreeting greeting="I'm a stateful class component!" age="42" /> */}
-      <StatefulGreetingWithPrevState />
+      {/* <StatefulGreetingWithPrevState /> */}
+      {/* <EventsFunctional /> */}
+      {/* <EventsClass /> */}
+      <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional connected={false} />
     </div>
   );
 }
---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,60 @@
+// rfce //
+
+import React from 'react'
+
+function ES7Snippets() {
+  return (
+    <div>ES7Snippets</div>
+  )
+}
+
+
+// rafce //
+
+export default ES7Snippets
+
+import React from 'react'
+
+const ES7Snippets = () => {
+  return (
+    <div>ES7Snippets</div>
+  )
+}
+
+
+// rce //
+
+export default ES7Snippets
+
+import React, { Component } from 'react'
+
+export class ES7Snippets extends Component {
+  render() {
+    return (
+      <div>ES7Snippets</div>
+    )
+  }
+}
+
+
+// rconst //
+
+export default ES7Snippets
+
+constructor(props) {
+  super(props)
+
+  this.state = {
+     first
+  }
+}
+
+
+// clg //
+
+console.log(first)
+
+
+// clo //
+
+console.log('first', first)
