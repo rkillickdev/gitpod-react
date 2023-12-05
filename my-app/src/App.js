0ee0525cb492f6637d56d386e32548f079143d7c
Rob Killick <robkillick@me.com>
2023-12-05 15:48:19 +0000

    add SearchBar component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,7 +12,10 @@ import './App.css';
 // import RenderingLists from './components/RenderingLists';
 // import LifeCyclesCDM from './components/LifeCyclesCDM';
 // import LifeCyclesCDU from './components/LifeCyclesCDU';
-import LifeCyclesCWU from './components/LifeCyclesCWU';
+// import LifeCyclesCWU from './components/LifeCyclesCWU';
+// import ControlledForm from './components/ControlledForm';
+// import UncontrolledForm from './components/UncontrolledForm';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
@@ -29,7 +32,10 @@ function App() {
       {/* <RenderingLists /> */}
       {/* <LifeCyclesCDM /> */}
       {/* <LifeCyclesCDU /> */}
-      <LifeCyclesCWU />
+      {/* <LifeCyclesCWU /> */}
+      {/* <ControlledForm /> */}
+      {/* <UncontrolledForm /> */}
+      <SearchBar />
     </div>
   );
 }
---
+++ my-app/src/components/ControlledForm.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+
+export class ControlledForm extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       name: '',
+       category: 'website',
+       comments: ''
+    }
+  } 
+  
+  handleNameChange = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+  handleCategoryChange = (event) => {
+    this.setState({
+      category: event.target.value
+    })
+  }
+  handleCommentsChange = (event) => {
+    this.setState({
+      comments: event.target.value
+    })
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Please fill in the form below:</h2>
+        <form onSubmit={this.handleSubmit}>
+            <div>
+              <label htmlFor="id-name">Your Name:</label>
+              <input
+                  onChange={this.handleNameChange}
+                  value={this.state.name} 
+                  id='id-name' name='name' 
+                  type='text' 
+              />
+            </div>
+            <div>
+              <label htmlFor="id-category">Query Category:</label>
+              <select 
+                id='id-category' 
+                name='category'
+                value={this.state.category}
+                onChange={this.handleCategoryChange}
+              >
+                <option value="website">Website Issue</option>
+                <option value="order">Order Issue</option>
+                <option value="general">General Enquiry</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="id-comments">Comments:</label>
+              <textarea
+                  id="id-comments"
+                  name="comments"
+                  value={this.state.comments}
+                  onChange={this.handleCommentsChange}
+              />
+            </div>
+            <input type='submit' value='submit' />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default ControlledForm
